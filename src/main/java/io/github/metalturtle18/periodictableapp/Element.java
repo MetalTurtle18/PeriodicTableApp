e8a6bb11ef5e7abe6a55a8228f2af377dd1a8264
MetalTurtle <dekolis1218@gmail.com>
2021-05-11 21:44:39 -0700

    Add state of matter and metal type properties to elements

--- src/main/java/io/github/metalturtle18/periodictableapp/Element.java
+++ src/main/java/io/github/metalturtle18/periodictableapp/Element.java
@@ -2,24 +2,24 @@ package io.github.metalturtle18.periodictableapp;
 
 public enum Element {
 
-    HYDROGEN("Hydrogen", "H", 1, 1, 1, 1.008, 2.20),
-    HELIUM("Helium", "He", 2, 18, 1, 4.003, -1D),
+    HYDROGEN("Hydrogen", "H", 1, 1, 1, 1.008, 2.20, MatterState.GAS, MetalType.NONMETAL),
+    HELIUM("Helium", "He", 2, 18, 1, 4.003, -1, MatterState.GAS, MetalType.NONMETAL),
     LITHIUM("Lithium", "Li", 3, 1, 2, 6.94, 0.98),
     BERYLLIUM("Beryllium", "Be", 4, 2, 2, 9.012, 1.57),
-    BORON("Boron", "B", 5, 13, 2, 10.81, 2.04),
-    CARBON("Carbon", "C", 6, 14, 2, 12.01, 2.55),
-    NITROGEN("Nitrogen", "N", 7, 15, 2, 14.01, 3.04),
-    OXYGEN("Oxygen", "O", 8, 16, 2, 16.00, 3.44),
-    FLUORINE("Fluorine", "F", 9, 17, 2, 19.00, 3.98),
-    NEON("Neon", "Ne", 10, 18, 2, 20.18, -1D),
+    BORON("Boron", "B", 5, 13, 2, 10.81, 2.04, MetalType.METALLOID),
+    CARBON("Carbon", "C", 6, 14, 2, 12.01, 2.55, MetalType.NONMETAL),
+    NITROGEN("Nitrogen", "N", 7, 15, 2, 14.01, 3.04, MatterState.GAS, MetalType.NONMETAL),
+    OXYGEN("Oxygen", "O", 8, 16, 2, 16.00, 3.44, MatterState.GAS, MetalType.NONMETAL),
+    FLUORINE("Fluorine", "F", 9, 17, 2, 19.00, 3.98, MatterState.GAS, MetalType.NONMETAL),
+    NEON("Neon", "Ne", 10, 18, 2, 20.18, -1, MatterState.GAS, MetalType.NONMETAL),
     SODIUM("Sodium", "Na", 11, 1, 3, 22.99, 0.93),
     MAGNESIUM("Magnesium", "Mg", 12, 2, 3, 24.30, 1.31),
     ALUMINUM("Aluminum", "Al", 13, 13, 3, 26.98, 1.61),
-    SILICON("Silicon", "Si", 14, 14, 3, 28.09, 1.90),
-    PHOSPHORUS("Phosphorus", "P", 15, 15, 3, 30.97, 2.19),
-    SULFUR("Sulfur", "S", 16, 16, 3, 32.06, 2.58),
-    CHLORINE("Chlorine", "Cl", 17, 17, 3, 35.45, 3.16),
-    ARGON("Argon", "Ar", 18, 18, 3, 39.95, -1D),
+    SILICON("Silicon", "Si", 14, 14, 3, 28.09, 1.90, MetalType.METALLOID),
+    PHOSPHORUS("Phosphorus", "P", 15, 15, 3, 30.97, 2.19, MetalType.NONMETAL),
+    SULFUR("Sulfur", "S", 16, 16, 3, 32.06, 2.58, MetalType.NONMETAL),
+    CHLORINE("Chlorine", "Cl", 17, 17, 3, 35.45, 3.16, MatterState.GAS, MetalType.NONMETAL),
+    ARGON("Argon", "Ar", 18, 18, 3, 39.95, -1, MatterState.GAS, MetalType.NONMETAL),
     POTASSIUM("Potassium", "K", 19, 1, 4, 39.10, 0.82),
     CALCIUM("Calcium", "Ca", 20, 2, 4, 40.08, 1.00),
     SCANDIUM("Scandium", "Sc", 21, 3, 4, 44.96, 1.36),
@@ -33,11 +33,11 @@ public enum Element {
     COPPER("Copper", "Cu", 29, 11, 4, 63.55, 1.90),
     ZINC("Zinc", "Zn", 30, 12, 4, 65.38, 1.65),
     GALLIUM("Gallium", "Ga", 31, 13, 4, 69.72, 1.81),
-    GERMANIUM("Germanium", "Ge", 32, 14, 4, 72.63, 2.01),
-    ARSENIC("Arsenic", "As", 33, 15, 4, 74.92, 2.18),
-    SELENIUM("Selenium", "Se", 34, 16, 4, 78.97, 2.55),
-    BROMINE("Bromine", "Br", 35, 17, 4, 79.90, 2.96),
-    KRYPTON("Krypton", "Kr", 36, 18, 4, 83.80, 3.00),
+    GERMANIUM("Germanium", "Ge", 32, 14, 4, 72.63, 2.01, MetalType.METALLOID),
+    ARSENIC("Arsenic", "As", 33, 15, 4, 74.92, 2.18, MetalType.METALLOID),
+    SELENIUM("Selenium", "Se", 34, 16, 4, 78.97, 2.55, MetalType.NONMETAL),
+    BROMINE("Bromine", "Br", 35, 17, 4, 79.90, 2.96, MatterState.LIQUID, MetalType.NONMETAL),
+    KRYPTON("Krypton", "Kr", 36, 18, 4, 83.80, 3.00, MatterState.GAS, MetalType.NONMETAL),
     RUBIDIUM("Rubidium", "Rb", 37, 1, 5, 85.47, 0.82),
     STRONTIUM("Strontium", "Sr", 38, 2, 5, 87.62, 0.95),
     YTTRIUM("Yttrium", "Y", 39, 3, 5, 88.91, 1.22),
@@ -52,10 +52,10 @@ public enum Element {
     CADMIUM("Cadmium", "Cd", 48, 12, 5, 112.4, 1.69),
     INDIUM("Indium", "In", 49, 13, 5, 114.8, 1.78),
     TIN("Tin", "Sn", 50, 14, 5, 118.7, 1.96),
-    ANTIMONY("Antimony", "Sb", 51, 15, 5, 121.8, 2.05),
-    TELLURIUM("Tellurium", "Te", 52, 16, 5, 127.6, 2.1),
-    IODINE("Iodine", "I", 53, 17, 5, 126.9, 2.66),
-    XENON("Xenon", "Xe", 54, 18, 5, 131.3, 2.60),
+    ANTIMONY("Antimony", "Sb", 51, 15, 5, 121.8, 2.05, MetalType.METALLOID),
+    TELLURIUM("Tellurium", "Te", 52, 16, 5, 127.6, 2.1, MetalType.METALLOID),
+    IODINE("Iodine", "I", 53, 17, 5, 126.9, 2.66, MetalType.NONMETAL),
+    XENON("Xenon", "Xe", 54, 18, 5, 131.3, 2.60, MatterState.GAS, MetalType.NONMETAL),
     CAESIUM("Caesium", "Cs", 55, 1, 6, 132.9, 0.79),
     BARIUM("Barium", "Ba", 56, 2, 6, 137.3, 0.89),
     LANTHANUM("Lanthanum", "La", 57, -1, 6, 138.9, 1.1),
@@ -81,13 +81,13 @@ public enum Element {
     IRIDIUM("Iridium", "Ir", 77, 9, 6, 192.2, 2.20),
     PLATINUM("Platinum", "Pt", 78, 10, 6, 195.1, 2.28),
     GOLD("Gold", "Au", 79, 11, 6, 197.0, 2.54),
-    MERCURY("Mercury", "Hg", 80, 12, 6, 200.6, 2.00),
+    MERCURY("Mercury", "Hg", 80, 12, 6, 200.6, 2.00, MatterState.LIQUID),
     THALLIUM("Thallium", "Tl", 81, 13, 6, 204.4, 1.62),
     LEAD("Lead", "Pb", 82, 14, 6, 207.2, 1.87),
     BISMUTH("Bismuth", "Bi", 83, 15, 6, 209.0, 2.02),
     POLONIUM("Polonium", "Po", 84, 16, 6, 209, 2.0),
-    ASTATINE("Astatine", "At", 85, 17, 6, 210, 2.2),
-    RADON("Radon", "Rn", 86, 18, 6, 222, 2.2),
+    ASTATINE("Astatine", "At", 85, 17, 6, 210, 2.2, MetalType.METALLOID),
+    RADON("Radon", "Rn", 86, 18, 6, 222, 2.2, MatterState.GAS, MetalType.NONMETAL),
     FRANCIUM("Francium", "Fr", 87, 1, 7, 223, 0.79),
     RADIUM("Radium", "Ra", 88, 2, 7, 226, 0.9),
     ACTINIUM("Actinium", "Ac", 89, -1, 7, 227, 1.1),
@@ -110,19 +110,38 @@ public enum Element {
     SEABORGIUM("Seaborgium", "Sg", 106, 6, 7, 269, -1),
     BOHRIUM("Bohrium", "Bh", 107, 7, 7, 270, -1),
     HASSIUM("Hassium", "Hs", 108, 8, 7, 270, -1),
-    MEITNERIUM("Meitnerium", "Mt", 109, 9, 7, 278, -1),
-    DARMSTADTIUM("Darmstadtium", "Ds", 110, 10, 7, 281, -1),
-    ROENTGENIUM("Roentgenium", "Rg", 111, 11, 7, 282, -1),
+    MEITNERIUM("Meitnerium", "Mt", 109, 9, 7, 278, -1, MetalType.OTHER),
+    DARMSTADTIUM("Darmstadtium", "Ds", 110, 10, 7, 281, -1, MetalType.OTHER),
+    ROENTGENIUM("Roentgenium", "Rg", 111, 11, 7, 282, -1, MetalType.OTHER),
     COPERNICIUM("Copernicium", "Cn", 112, 12, 7, 285, -1),
-    NIHONIUM("Nihonium", "Nh", 113, 13, 7, 286, -1),
-    FLEROVIUM("Flerovium", "Fl", 114, 14, 7, 289, -1),
-    MOSCOVIUM("Moscovium", "Mc", 115, 15, 7, 290 ,-1),
-    LIVERMORIUM("Livermorium", "Lv", 116, 16, 7, 293, -1),
-    TENNESSINE("Tennessine", "Ts", 117, 17, 7, 294, -1),
-    OGANESSON("Oganesson", "Og", 118, 18, 7, 294, -1);
+    NIHONIUM("Nihonium", "Nh", 113, 13, 7, 286, -1, MetalType.OTHER),
+    FLEROVIUM("Flerovium", "Fl", 114, 14, 7, 289, -1, MetalType.OTHER),
+    MOSCOVIUM("Moscovium", "Mc", 115, 15, 7, 290 ,-1, MetalType.OTHER),
+    LIVERMORIUM("Livermorium", "Lv", 116, 16, 7, 293, -1, MetalType.OTHER),
+    TENNESSINE("Tennessine", "Ts", 117, 17, 7, 294, -1, MetalType.OTHER),
+    OGANESSON("Oganesson", "Og", 118, 18, 7, 294, -1, MetalType.OTHER);
 
     /**
-     * These are the properties of each element. If any one is -1 that means it does not exist
+     * States of matter
+     */
+    enum MatterState {
+        SOLID,
+        LIQUID,
+        GAS
+    }
+
+    /**
+     * Metal types of elements
+     */
+    enum MetalType {
+        METAL,
+        NONMETAL,
+        METALLOID,
+        OTHER
+    }
+
+    /**
+     * These are the properties of each element. If any property is -1 that means it does not exist
      */
     public final String name;
     public final String symbol;
@@ -131,8 +150,10 @@ public enum Element {
     public final int period;
     public final double atomicWeight;
     public final double electronegativity;
+    public final MatterState stateAtRoomTemperature;
+    public final MetalType metalType;
 
-    Element(String name, String symbol, int atomicNumber, int group, int period, double atomicWeight, double electronegativity) {
+    Element(String name, String symbol, int atomicNumber, int group, int period, double atomicWeight, double electronegativity, MatterState stateAtRoomTemperature, MetalType metalType) {
         this.name = name;
         this.symbol = symbol;
         this.atomicNumber = atomicNumber;
@@ -140,5 +161,23 @@ public enum Element {
         this.period = period;
         this.atomicWeight = atomicWeight;
         this.electronegativity = electronegativity;
+        this.stateAtRoomTemperature = stateAtRoomTemperature;
+        this.metalType = metalType;
+    }
+
+    /**
+     * Different constructors with defaults because a lot of the elements are solid metals
+     */
+
+    Element(String name, String symbol, int atomicNumber, int group, int period, double atomicWeight, double electronegativity) {
+        this(name, symbol, atomicNumber, group, period, atomicWeight, electronegativity, MatterState.SOLID, MetalType.METAL);
+    }
+
+    Element(String name, String symbol, int atomicNumber, int group, int period, double atomicWeight, double electronegativity, MatterState stateAtRoomTemperature) {
+        this(name, symbol, atomicNumber, group, period, atomicWeight, electronegativity, stateAtRoomTemperature, MetalType.METAL);
+    }
+
+    Element(String name, String symbol, int atomicNumber, int group, int period, double atomicWeight, double electronegativity, MetalType metalType) {
+        this(name, symbol, atomicNumber, group, period, atomicWeight, electronegativity, MatterState.SOLID, metalType);
     }
 }
